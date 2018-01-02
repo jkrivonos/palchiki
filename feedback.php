7c2af4d69533a2fb7e9c28a9cb8a7e822a08fdb0
jkrivonos <ja.krivonos@gmail.com>
2018-01-02 22:24:53 +0300

    refactoring feedback

--- feedback.php
+++ feedback.php
@@ -6,49 +6,35 @@
  * Time: 17:43
  */
 
-/* Ваш адрес и тема сообщения */
+$ADDRESS = 'mihasic@inbox.ru';
+$SUBJECT = 'Сообщение с сайта palchiki';
 
-$email = '';
-$tel = '';
-$message = '';
-$name = '';
-$bezspama = '';
+/**
+ * @return string
+ */
+function createMessage(): string
+{
+    $email = htmlspecialchars(trim($_POST["eeemail"]));
+    $tel = htmlspecialchars(trim($_POST["tel"]));
+    $message = htmlspecialchars(trim($_POST["message"]));
+    $name = htmlspecialchars(trim($_POST["name"]));
 
-$address = "mihasic@inbox.ru";
-$sub = "Сообщение с сайта palchiki";
-
-if (!empty($_POST["email"]) || !empty($_POST["tel"])) {
-
-    if (!empty($_POST["bezspama"])) {
-        $bezspama = htmlspecialchars(trim($_POST["bezspama"]));
-    }
-
-    if (empty($bezspama))
-    {
-        if (!empty($_POST["email"])) {
-            $email = htmlspecialchars(trim($_POST["email"]));
-        }
-
-        if (!empty($_POST["tel"])) {
-            $tel = htmlspecialchars(trim($_POST["tel"]));
-        }
-
-        if (!empty($_POST["message"])) {
-            $message = htmlspecialchars(trim($_POST["message"]));
-        }
-
-        if (!empty($_POST["name"])) {
-            $name = htmlspecialchars(trim($_POST["name"]));
-        }
-
-        $mes = "Сообщение с сайта palchiki.\n
+    $mes = "Сообщение с сайта palchiki.\n
         Имя отправителя: $name 
         Электронный адрес отправителя: $email
         Телефон отправителя: $tel
         Текст сообщения:
         $message";
+    return $mes;
+}
 
-        if (mail($address, $sub, $mes)) {
+if (!empty($_POST["email"]) || !empty($_POST["tel"])) {
+    $bezspama = htmlspecialchars(trim($_POST["bezspama"]));
+    if (empty($bezspama))
+    {
+        $mes = createMessage();
+
+        if (mail($ADDRESS, $SUBJECT, $mes)) {
             echo 'Мы с Вами свяжемся!';
         }
         else {
