4e036298406f5ca52f0bf2439309ac69bf432c6c
jkrivonos <ja.krivonos@gmail.com>
2018-01-10 00:36:41 +0300

    modify index-view for admin panel

--- admin/views/index.php
+++ admin/views/index.php
@@ -6,29 +6,55 @@
  * Time: 0:43
  */
 ?>
-<h4>Список предстоящих мастер-классов</h4>
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Наименование мастер-класса</th>
-        <th>Описание</th>
-        <th>Начало занятия</th>
-        <th>Стоимость, &#x20bd;</th>
-        <th></th>
-        <th></th>
-    </tr>
-    <?php foreach ($events as $event): ?>
-    <tr>
-        <td><?php echo $event['id']; ?></td>
-        <td><?php echo $event['master_name']; ?></td>
-        <td><?php echo $event['description']; ?></td>
-        <td><?php echo $event['date']; ?></td>
-        <td><?php echo $event['coast']; ?></td>
-        <td><a href="/admin/update/<?php echo $event['id']; ?>" title="редактировать">редактировать</a></td>
-        <td><a href="/admin/delete/<?php echo $event['id']; ?>" title="удалить">удалить</a></td>
-    </tr>
-    <?php endforeach; ?>
-</table>
-<form action="/admin/create/" method="post">
-    <input type="submit" name="submit" value="Добавить"></input>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lobster&amp;subset=cyrillic,cyrillic-ext" rel="stylesheet">
+    <script src="https://use.fontawesome.com/661a533fef.js"></script>
+    <title>Творческая Студия "Пальчики"</title>
+</head>
+<body>
+<div class="header">
+    <div class="logo"><a href="/index.html"><img src="/images/logo.png" alt="Пальчики"></a></div>
+    <ul class='menuInfo'>
+        <li><a href="/index.html">Главная</a></li>
+        <li><a href="/parties.html">Галерея</a></li>
+        <li><a href="/contacts.html">Контакты</a></li>
+        <li><a href="/forecast.html">Анонс</a></li>
+    </ul>
+</div>
+<p style="text-align: center">Список предстоящих мастер классов</p>
+<div class="flexTable">
+    <div class="allTable">
+        <div class="justTable">
+            <div class="tableTitle">
+                <div>ID</div>
+                <div>Название МК</div>
+                <div>Описание</div>
+                <div>Дата и время МК</div>
+                <div>Стоимость, &#x20BD;</div>
+                <div class="deletEdit"></div>
+            </div>
+            <?php foreach ($events as $event): ?>
+            <div class="tableContent">
+                <div><?php echo $event['id']; ?></div>
+                <div><?php echo $event['master_name']; ?></div>
+                <div><?php echo $event['description']; ?></div>
+                <div><?php echo $event['date']; ?></div>
+                <div><?php echo $event['coast']; ?></div>
+                <div class="deletEdit">
+                    <a href="/admin/update/<?php echo $event['id']; ?>" class="fa fa-pencil" aria-hidden="true"></a>
+                    <a href="/admin/delete/<?php echo $event['id']; ?>" class="fa fa-trash-o" aria-hidden="true"></a>
+                </div>
+            </div>
+        <?php endforeach; ?>
+        </div>
+    </div>
+    <form action="/admin/create/" method="post">
+        <input type="submit" name="submit" class="btnEnter admin" value="Добавить новый МК">
+    </form>
+</div>
+</body>
+</html>
